c2a7aba23673725fc8702a07c3b9cb231c3ece77
Joaobachmann <joaobachmannalun@gmail.com>
2026-03-09 15:11:07 -0300

    feat: criação da entidade para Livro

---
+++ core/views/livro.py
@@ -0,0 +1,8 @@
+from rest_framework.viewsets import ModelViewSet
+
+from core.models.livro import Livro
+from core.serializers.livro import LivroSerializer
+
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
